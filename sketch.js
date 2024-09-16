258768fc5455b6266cc85be9dc8530d0b8839da8
“Your <jbuech8@lsu.edugit config --global user.email jbuech8@lsu.edu>
2024-09-16 12:36:05 -0500

    ffff

---
+++ sketch.js
@@ -0,0 +1,75 @@
+function setup() {
+
+    createCanvas(400,269);
+
+}
+
+function draw() {
+
+// Red and Yellow background   
+    stroke(0)
+    background(221, 176, 26);
+    strokeWeight(0)
+    fill(137, 1, 0);
+    rect(0, 142, width, 180);
+
+// Black Lines on the Bottom    
+    fill(0);
+    strokeWeight(6.5);
+    line(0,145,400,140);
+    strokeWeight(7.5);
+    line(0,188,400,180);
+    line(0,220,400,215);
+
+// Cheeta Print
+    strokeWeight(2);
+    line(11,0,13,5);
+    line(0,5,8,7);
+    line(0,9,7,10);
+    ellipse(-115,-55,300,300);
+    ellipse(80,80,100,100);
+    stroke(221,176,26);
+    strokeWeight(16);
+    line(50,30,150,50);
+    strokeWeight(24);
+    line(20,108,120,128);
+    strokeWeight(0);
+    rect(76,117,49,25);
+    fill(221,176,26);
+    ellipse(84,76,50,47);
+    fill(0);
+    rect(169,126,55,20);
+    ellipse(160,0,80,120);
+    ellipse(240,20,80,120);
+    strokeWeight(10);
+    line(90,40,300,90);
+    strokeWeight(0);
+    ellipse(260,90,80,60);
+    ellipse(320,90,70,55);
+    fill(221,176,26);
+    ellipse(290,94,40,50);
+    fill(0);
+    ellipse(340,25,80,70);
+    stroke(221,176,26);
+    strokeWeight(16);
+    line(360,80,340,35);
+    line(410,80,340,35);
+    line(290,30,340,-20);
+    line(340,-10,400,15);
+    strokeWeight(9)
+    line(130,115,50,110)
+    strokeWeight(0);
+    fill(0);
+    ellipse(383,72,30,30);
+    fill(221, 176, 26)
+    ellipse(30,100,30,70)
+    ellipse(44,99,20,12)
+    strokeWeight(10)
+    line(490,140,370,48)
+    line(490,200,370,80)
+    line(215,50,180,40)
+    line(280,12,220,-10)
+    line(190,-5,204,0)
+
+    
+}
